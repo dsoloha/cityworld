c7dc2ef7d1657638f3062318f348649c69ec0760
DSoloha <dsoloha@live.com>
2023-03-09 14:38:41 -0500

    Added basic intro setup

---
+++ src/routes/(intro)/player.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import Button from '$components/Button.svelte'
+  import Card from '$components/Card.svelte'
+  import Input from '$components/Input.svelte'
+
+  import player from '$stores/player.store'
+  import see from '$stores/see.store'
+</script>
+
+<Card>
+  <p>
+    The people living there now aren't going to accept just anyone as a leader. <b>Who are you?</b>
+  </p>
+  <Input name="player-name" placeholder="your name" bind:value={$player.name} />
+  <Button
+    focus
+    handler={() => {
+      $see.setup.player = false
+    }}>Continue</Button
+  >
+</Card>
