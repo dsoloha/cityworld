67bab02dd4b37e5b6ac4b3c58bc6ae359ae47ae6
DSoloha <dsoloha@live.com>
2023-03-31 09:23:20 -0400

    Added Editable to dashboard

--- src/routes/(app)/dashboard.svelte
+++ src/routes/(app)/dashboard.svelte
@@ -1,10 +1,13 @@
 <script>
   import Card from '$components/Card.svelte'
+  import Editable from '$components/Editable.svelte'
 
   import city from '$stores/city.store'
 </script>
 
 <Card>
-  <h1>{$city.name}</h1>
+  <h1>
+    <Editable bind:value={$city.name} />
+  </h1>
   <p>dashboard goes here</p>
 </Card>
