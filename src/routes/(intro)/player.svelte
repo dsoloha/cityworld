30678c11d71bbce0cc089db81d16cdea506c3bed
DSoloha <dsoloha@live.com>
2023-04-04 00:01:26 -0400

    Added a basic occupation selector

--- src/routes/(intro)/player.svelte
+++ src/routes/(intro)/player.svelte
@@ -16,14 +16,86 @@
   <Input name="player-name" placeholder="your name" bind:value={$player.name} />
   {#if $player.name}
     <p>
-      Your name is <b>{$player.name}</b>.
+      <i>Your name is <b>{$player.name}</b>.</i>
     </p>
+    <p>And what did you do, <b>{$player.name}</b>?</p>
     <Button
-      focus
       handler={() => {
-        $see.setup.intro = false
-        $see.setup.player = false
-      }}>Continue</Button
+        $player.occupation = 'farmer'
+      }}
+      tooltip="You used to be a farmer.">Agriculture</Button
     >
+    <Button
+      handler={() => {
+        $player.occupation = 'construction'
+      }}
+      tooltip="You used to own a successful construction company.">Construction</Button
+    >
+    <Button
+      handler={() => {
+        $player.occupation = 'art'
+      }}
+      tooltip="You used to be a freelance artist.">Art</Button
+    >
+    <Button
+      handler={() => {
+        $player.occupation = 'finance'
+      }}
+      tooltip="You used to be a business manager.">Finance</Button
+    >
+    <Button
+      handler={() => {
+        $player.occupation = 'education'
+      }}
+      tooltip="You were once the dean of a prestigious college.">Education</Button
+    >
+    <Button
+      handler={() => {
+        $player.occupation = 'politics'
+      }}
+      tooltip="You used to be a politician.">Politics</Button
+    >
+    <Button
+      handler={() => {
+        $player.occupation = 'science'
+      }}
+      tooltip="You used to be a scientist.">Science</Button
+    >
+    <Button
+      handler={() => {
+        $player.occupation = 'tech'
+      }}
+      tooltip="You used to work in the technology sector.">Tech</Button
+    >
+    <Button
+      handler={() => {
+        $player.occupation = 'military'
+      }}
+      tooltip="You were once career military.">Military</Button
+    >
+    <Button
+      handler={() => {
+        $player.occupation = 'religion'
+      }}
+      tooltip="You once led a large congregation.">Religious</Button
+    >
+    <Button
+      handler={() => {
+        $player.occupation = 'crime'
+      }}
+      tooltip="You were a career criminal.">Criminal</Button
+    >
+    {#if $player.occupation}
+      <p>
+        <i>You used to {$player.occupation}.</i>
+      </p>
+      <Button
+        focus
+        handler={() => {
+          $see.setup.intro = false
+          $see.setup.player = false
+        }}>Continue</Button
+      >
+    {/if}
   {/if}
 </Card>
