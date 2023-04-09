1bbf133d249f6dc999dfacc35908ed155448a469
DSoloha <dsoloha@live.com>
2023-04-09 00:21:55 -0400

    Added basic setup options for player sex, height, and weight

--- src/routes/(intro)/player.svelte
+++ src/routes/(intro)/player.svelte
@@ -27,6 +27,7 @@
     >
   </p>
   <Input name="player-name-first" placeholder="first name" bind:value={$player.name.first} />
+
   {#if $player.name.first}
     {#if $player.name.last}
       <Input name="player-name-middle" placeholder="middle name" bind:value={$player.name.middle} />
@@ -35,93 +36,128 @@
     <p class="player-response">
       Your name is <b>{name}</b>.
     </p>
-    <p>And what did you do, <b>{first}</b>?</p>
-    <div class="center-container">
-      <div class="center">
-        <Button
-          handler={() => {
-            $player.occupation = 'farmer'
-          }}
-          tooltip="You used to be a farmer.">Agriculture</Button
-        >
-        <Button
-          handler={() => {
-            $player.occupation = 'construction'
-          }}
-          tooltip="You used to own a successful construction company.">Construction</Button
-        >
-        <Button
-          handler={() => {
-            $player.occupation = 'art'
-          }}
-          tooltip="You used to be a freelance artist.">Art</Button
-        >
-        <Button
-          handler={() => {
-            $player.occupation = 'finance'
-          }}
-          tooltip="You used to be a business manager.">Finance</Button
-        >
-        <Button
-          handler={() => {
-            $player.occupation = 'education'
-          }}
-          tooltip="You were once the dean of a prestigious college.">Education</Button
-        >
-        <Button
-          handler={() => {
-            $player.occupation = 'politics'
-          }}
-          tooltip="You used to be a politician.">Politics</Button
-        >
-        <Button
-          handler={() => {
-            $player.occupation = 'science'
-          }}
-          tooltip="You used to be a scientist.">Science</Button
-        >
-        <Button
-          handler={() => {
-            $player.occupation = 'tech'
-          }}
-          tooltip="You used to work in the technology sector.">Tech</Button
-        >
-        <Button
-          handler={() => {
-            $player.occupation = 'military'
-          }}
-          tooltip="You were once career military.">Military</Button
-        >
-        <Button
-          handler={() => {
-            $player.occupation = 'religion'
-          }}
-          tooltip="You once led a large congregation.">Religious</Button
-        >
-        <Button
-          handler={() => {
-            $player.occupation = 'crime'
-          }}
-          tooltip="You were a career criminal.">Criminal</Button
-        >
-      </div>
+
+    <p class="margin-top"><b>And what did you do, {first}?</b></p>
+    <div>
+      <Button
+        handler={() => {
+          $player.occupation = 'farmer'
+        }}
+        tooltip="You used to be a farmer.">Agriculture</Button
+      >
+      <Button
+        handler={() => {
+          $player.occupation = 'construction'
+        }}
+        tooltip="You used to own a successful construction company.">Construction</Button
+      >
+      <Button
+        handler={() => {
+          $player.occupation = 'art'
+        }}
+        tooltip="You used to be a freelance artist.">Art</Button
+      >
+      <Button
+        handler={() => {
+          $player.occupation = 'finance'
+        }}
+        tooltip="You used to be a business manager.">Finance</Button
+      >
+      <Button
+        handler={() => {
+          $player.occupation = 'education'
+        }}
+        tooltip="You were once the dean of a prestigious college.">Education</Button
+      >
+      <Button
+        handler={() => {
+          $player.occupation = 'politics'
+        }}
+        tooltip="You used to be a politician.">Politics</Button
+      >
+      <Button
+        handler={() => {
+          $player.occupation = 'science'
+        }}
+        tooltip="You used to be a scientist.">Science</Button
+      >
+      <Button
+        handler={() => {
+          $player.occupation = 'tech'
+        }}
+        tooltip="You used to work in the technology sector.">Tech</Button
+      >
+      <Button
+        handler={() => {
+          $player.occupation = 'military'
+        }}
+        tooltip="You were once career military.">Military</Button
+      >
+      <Button
+        handler={() => {
+          $player.occupation = 'religion'
+        }}
+        tooltip="You once led a large congregation.">Religious</Button
+      >
+      <Button
+        handler={() => {
+          $player.occupation = 'crime'
+        }}
+        tooltip="You were a career criminal.">Criminal</Button
+      >
     </div>
+
     {#if $player.occupation}
       <p class="player-response">
         You used to <b>{$player.occupation}</b>.
       </p>
-      <p>
-        Now describe yourself, <b>{first}</b>.
+
+      <p class="margin-top">
+        Now describe yourself a little, <b>{first}</b>. <b>Are you a male or female?</b>
       </p>
+      <div>
+        <Button
+          handler={() => {
+            $player.sex = 'male'
+          }}>Male</Button
+        >
+        <Button
+          handler={() => {
+            $player.sex = 'female'
+          }}>Female</Button
+        >
+      </div>
+
+      {#if $player.sex}
+        <p class="player-response">
+          You are a <b>{$player.sex}</b>.
+        </p>
+
+        <p class="margin-top"><b>How tall are you?</b></p>
+        <Input name="height" placeholder="height" bind:value={$player.height} />cm
+        <p class="player-response">
+          You are <b>{$player.height}cm</b> tall.
+        </p>
+
+        <p class="margin-top"><b>How much do you weigh?</b></p>
+        <Input name="weight" placeholder="weight" bind:value={$player.weight} />kg
+        <p class="player-response">
+          You weigh <b>{$player.weight}kg</b>.
+        </p>
+      {/if}
     {/if}
   {/if}
-  {#if $player.name.first && $player.occupation}
-    <Button
-      focus
-      handler={() => {
-        $see.setup.intro = false
-        $see.setup.player = false
-      }}>Continue</Button
-    >
+
+  {#if $player.name.first && $player.occupation && $player.sex && $player.height && $player.weight}
+    <div class="margin-top">
+      <Button
+        focus
+        handler={() => {
+          $see.setup.intro = false
+          $see.setup.player = false
+          $see.ui = true
+        }}>Continue</Button
+      >
+    </div>
   {/if}
 </Card>
