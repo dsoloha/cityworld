109cdc4fe4dd1943e48184f57ae2d171f2f0acc7
DSoloha <dsoloha@live.com>
2023-04-16 02:24:13 -0400

    Added City class

--- src/classes/City.ts
+++ src/classes/City.ts
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
 import common from '$stores/common.store.ts'
+=======
+import global from '$stores/global.store'
+>>>>>>> 19b3735 (Added City class)
 import { get } from 'svelte/store'
 
 export default class City {
@@ -6,9 +10,16 @@ export default class City {
   name: string
 
   constructor(name?: string) {
+<<<<<<< HEAD
     this.id = get(common).id
     this.name = name ?? ''
 
     common.set({ id: this.id++ })
+=======
+    this.id = get(global).id
+    this.name = name ?? ''
+
+    global.set({ id: this.id++ })
+>>>>>>> 19b3735 (Added City class)
   }
 }
