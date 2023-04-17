1f8a71d1e9f707ee569ea815f2c30927f8822528
DSoloha <dsoloha@live.com>
2023-04-17 09:45:21 -0400

    Added countries and continents

--- src/routes/(intro)/city.svelte
+++ src/routes/(intro)/city.svelte
@@ -6,6 +6,190 @@
   import city from '$stores/city.store'
   import player from '$stores/player.store'
   import see from '$stores/see.store'
+
+  const continents = ['Africa', 'Asia', 'Australia', 'Europe', 'North America', 'South America']
+  const countries = [
+    [
+      'Africa',
+      [
+        'Algeria',
+        'Angola',
+        'Benin',
+        'Botswana',
+        'Burkina Faso',
+        'Burundi',
+        'Cabo Verde',
+        'Cameroon',
+        'Central African Republic',
+        'Chad',
+        'Comoros',
+        'Congo',
+        'Djibouti',
+        'Democratic Republic of the Congo',
+        'Egypt',
+        'Equatorial Guinea',
+        'Eritrea',
+        'Eswatini',
+        'Ethiopia',
+        'Gabon',
+        'Gambia',
+        'Ghana',
+        'Guinea-Bissau',
+        'Guinea',
+        'Kenya',
+        'Lesotho',
+        'Liberia',
+        'Libya',
+        'Madagascar',
+        'Malawi',
+        'Mali',
+        'Mauritania',
+        'Mauritius',
+        'Morocco',
+        'Mozambique',
+        'Namibia',
+        'Niger',
+        'Nigeria',
+        'Rwanda',
+        'Sao Tome & Principe',
+        'Senegal',
+        'Seychelles',
+        'Sierra',
+        'Somalia',
+        'South',
+        'South',
+        'Sudan',
+        'Tanzania',
+        'Togo',
+        'Tunisia',
+        'Uganda',
+        'Zambia',
+        'Zimbabwe',
+        "Côte d'Ivoire",
+      ],
+    ],
+    [
+      'Asia',
+      [
+        'Afghanistan',
+        'Armenia',
+        'Azerbaijan',
+        'Bahrain',
+        'Bangladesh',
+        'Bhutan',
+        'Brunei',
+        'Cambodia',
+        'China',
+        'Cyprus',
+        'Georgia',
+        'India',
+        'Indonesia',
+        'Iran',
+        'Iraq',
+        'Israel',
+        'Japan',
+        'Jordan',
+        'Kazakhstan',
+        'Kuwait',
+        'Kyrgyzstan',
+        'Laos',
+        'Lebanon',
+        'Malaysia',
+        'Maldives',
+        'Mongolia',
+        'Myanmar',
+        'Nepal',
+        'North Korea',
+        'Oman',
+        'Pakistan',
+        'Philippines',
+        'Qatar',
+        'Saudi Arabia',
+        'Singapore',
+        'South',
+        'Sri Lanka',
+        'State',
+        'Syria',
+        'Tajikistan',
+        'Thailand',
+        'Timor-Leste',
+        'Turkey',
+        'Turkmenistan',
+        'United Arab Emirates',
+        'Uzbekistan',
+        'Vietnam',
+        'Yemen',
+      ],
+    ],
+    ['Australia', ['Australia']],
+    [
+      'Europe',
+      [
+        'Albania',
+        'Andorra',
+        'Austria',
+        'Belarus',
+        'Belgium',
+        'Bosnia and Herzegovina',
+        'Bulgaria',
+        'Croatia',
+        'Czech Republic',
+        'Denmark',
+        'Estonia',
+        'Finland',
+        'France',
+        'Germany',
+        'Greece',
+        'Holy See',
+        'Hungary',
+        'Iceland',
+        'Ireland',
+        'Italy',
+        'Latvia',
+        'Liechtenstein',
+        'Lithuania',
+        'Luxembourg',
+        'Malta',
+        'Moldova',
+        'Monaco',
+        'Montenegro',
+        'Netherlands',
+        'North',
+        'Norway',
+        'Poland',
+        'Portugal',
+        'Romania',
+        'Russia',
+        'San Marino',
+        'Serbia',
+        'Slovakia',
+        'Slovenia',
+        'Spain',
+        'Sweden',
+        'Switzerland',
+        'Ukraine',
+        'United',
+      ],
+    ],
+    ['North America', ['Canada', 'United States', 'Mexico']],
+    [
+      'South America',
+      [
+        'Argentina',
+        'Bolivia',
+        'Brazil',
+        'Chile',
+        'Colombia',
+        'Ecuador',
+        'Guyana',
+        'Paraguay',
+        'Peru',
+        'Suriname',
+        'Uruguay',
+        'Venezuela',
+      ],
+    ],
+  ]
 </script>
 
 <Card>
@@ -25,18 +209,51 @@
       This city is called <b>{$city.name}</b>.
     </p>
 
-    <p class="margin-top"><b>Where is <b>{$city.name}</b> located?</b></p>
-    <Input name="continent" placeholder="continent" />
-    <Input name="country" placeholder="country" />
+    <p class="margin-top"><b>On which continent is <b>{$city.name}</b> located?</b></p>
+
+    {#each continents as continent}
+      <Button
+        handler={() => {
+          $city.continent = continent
+        }}
+        focus={$city.continent == continent}>{continent}</Button
+      >
+    {/each}
+
+    {#if $city.continent}
+      <div class="margin-top">
+        <p><b>In which country?</b></p>
+
+        {#each countries as continent}
+          {#if continent[0] == $city.continent}
+            {#each continent[1] as country}
+              <Button
+                handler={() => {
+                  $city.country = country
+                }}
+                focus={$city.country == country}>{country}</Button
+              >
+            {/each}
+          {/if}
+        {/each}
+      </div>
+    {/if}
   {/if}
 
   {#if $city.name}
     <p class="margin-top player-response">
       This is the city of <b>{$city.name}</b>.
+      {#if $city.continent}
+        It is located
+        {#if $city.country}
+          in the country of <b>{$city.country}</b>,
+        {/if}
+        on the continent of <b>{$city.continent}</b>.
+      {/if}
     </p>
   {/if}
 
-  {#if $city.name}
+  {#if $city.name && $city.continent && $city.country}
     <div class="margin-top">
       <Button
         focus
