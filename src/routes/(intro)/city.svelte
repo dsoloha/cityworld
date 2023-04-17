8eb84313e5dc052fcb3a089b2eab09dc97001d5a
DSoloha <dsoloha@live.com>
2023-04-16 23:20:52 -0400

    Added basic city setup page

---
+++ src/routes/(intro)/city.svelte
@@ -0,0 +1,32 @@
+<script>
+  import Button from '$components/Button.svelte'
+  import Card from '$components/Card.svelte'
+  import Input from '$components/Input.svelte'
+
+  import city from '$stores/city.store'
+  import player from '$stores/player.store'
+  import see from '$stores/see.store'
+</script>
+
+<Card>
+  <p>
+    It seems the people here were impressed with what you told them, {$player.name.first}, and have
+    decided to give you a chance as leader.
+  </p>
+  <p>
+    But where <i>is</i> "here"?
+  </p>
+  <Input name="city-name" placeholder="city name" bind:value={$city.name} />
+
+  <div class="margin-top">
+    <Button
+      focus
+      handler={() => {
+        $see.setup.city = false
+        $see.setup.intro = false
+        $see.setup.player = false
+        $see.ui = true
+      }}>Continue</Button
+    >
+  </div>
+</Card>
