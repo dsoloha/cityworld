243a4ff12c1ec873e00b60872c2fdf5a8b993f78
DSoloha <dsoloha@live.com>
2023-04-18 09:07:17 -0400

    Added control properties

--- src/components/Select.svelte
+++ src/components/Select.svelte
@@ -1,5 +1,28 @@
-<script>
+<script lang="ts">
+  // from https://github.com/rob-balfre/svelte-select
   import Select from 'svelte-select'
+
+  export let disabled = false
+  export let filterText: string
+  export let focused = false
+  export let items: any[]
+  export let multiple = false
+  export let name: string
+  export let placeholder: string
+  export let required = false
+  export let searchable = true
+  export let value: any
 </script>
 
-<Select />
+<Select
+  {items}
+  {value}
+  {filterText}
+  {placeholder}
+  {disabled}
+  {multiple}
+  {searchable}
+  {focused}
+  {required}
+  {name}
+/>
