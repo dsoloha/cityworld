06daac83b97a139d4cd0c6b18b8dab58942ca242
DSoloha <dsoloha@live.com>
2023-04-20 11:04:38 -0400

    Added missing documentation

--- src/classes/Actor.ts
+++ src/classes/Actor.ts
@@ -2,18 +2,24 @@ import common from '$stores/common.store'
 import { get } from 'svelte/store'
 
 export default class Actor {
-  /** How tall the actor is, in cm. */
+  /** The actor's height, in cm. */
   height: number
   /** The actor's ID. */
   id: number
   /** The actor's name. */
   name: {
+    /** The actor's first name. */
     first: string
+    /** The actor's middle name. */
     middle: string
+    /** The actor's last name. */
     last: string
   }
+  /** The actor's occupation or previous occupation. */
   occupation: string
+  /** The actor's biological sex. */
   sex: string
+  /** The actor's weight, in kg. */
   weight: number
 
   constructor(name?: { first: string; middle: string; last: string }) {
