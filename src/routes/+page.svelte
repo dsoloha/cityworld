588b41b11e4ec0b2c28d4b24da2b04dbc12e888a
DSoloha <dsoloha@live.com>
2023-05-02 23:28:22 -0400

    Added placeholder login page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,12 @@
 <script>
-  import Dashboard from './(app)/dashboard.svelte'
-  import Intro from './(intro)/intro.svelte'
-  import Player from './(intro)/player.svelte'
-
   import see from '$stores/see.store'
+
+  import Dashboard from './(app)/dashboard.svelte'
+
   import City from './(intro)/city.svelte'
+  import Intro from './(intro)/intro.svelte'
+  import Login from './(intro)/login.svelte'
+  import Player from './(intro)/player.svelte'
 </script>
 
 <svelte:head>
@@ -14,6 +16,8 @@
 
 {#if $see.setup.intro}
   <Intro />
+{:else if $see.setup.login}
+  <Login />
 {:else if $see.setup.player}
   <Player />
 {:else if $see.setup.city}
