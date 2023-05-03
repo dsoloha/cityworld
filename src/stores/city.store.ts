c8057326811976896389ed6a6523e9ce027b7592
DSoloha <dsoloha@live.com>
2023-05-02 22:43:41 -0400

    Player city is now added to cities store

--- src/stores/city.store.ts
+++ src/stores/city.store.ts
@@ -1,6 +1,9 @@
-import { writable } from 'svelte/store'
 import City from '$classes/City'
+import { get, writable } from 'svelte/store'
+import cities from './cities.store'
 
 const city = writable(new City())
+
+cities.update((state) => state.set(get(city).ID, city))
 
 export default city
