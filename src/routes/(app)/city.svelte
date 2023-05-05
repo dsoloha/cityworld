6142cc8e7c38986f11a9dab9740d8fc36dd38f97
DSoloha <dsoloha@live.com>
2023-05-05 09:14:45 -0400

    Split dashboard into components

---
+++ src/routes/(app)/city.svelte
@@ -0,0 +1,11 @@
+<script>
+  import Card from '$components/Card.svelte'
+
+  import city from '$stores/city.store'
+</script>
+
+<Card>
+  <p>
+    Your city, {$city.name}, is located in what was once known as {$city.country}.
+  </p>
+</Card>
