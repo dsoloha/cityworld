ddb5ea2d4e5d5a80ff95c16211e05229a7bdb66f
DSoloha <dsoloha@live.com>
2023-12-28 16:09:19 -0500

    Replaced countries with regions

--- src/routes/(intro)/city.svelte
+++ src/routes/(intro)/city.svelte
@@ -1,284 +1,162 @@
 <script lang="ts">
-  import Button from '$components/Button.svelte'
-  import Card from '$components/Card.svelte'
-  import Input from '$components/Input.svelte'
-  import Select from '$components/Select.svelte'
+    import type { Continent, Region, Terrain } from '$classes/City'
 
-  import city from '$stores/city.store'
-  import player from '$stores/player.store'
-  import see from '$stores/see.store'
+    import Button from '$components/Button.svelte'
+    import Card from '$components/Card.svelte'
+    import Input from '$components/Input.svelte'
+    import Select from '$components/Select.svelte'
 
-  const continents = ['Africa', 'Asia', 'Australia', 'Europe', 'North America', 'South America']
-  const countries = new Map([
-    [
-      'Africa',
-      [
-        'Algeria',
-        'Angola',
-        'Benin',
-        'Botswana',
-        'Burkina Faso',
-        'Burundi',
-        'Cabo Verde',
-        'Cameroon',
-        'Central African Republic',
-        'Chad',
-        'Comoros',
-        'Congo',
-        'Djibouti',
-        'Democratic Republic of the Congo',
-        'Egypt',
-        'Equatorial Guinea',
-        'Eritrea',
-        'Eswatini',
-        'Ethiopia',
-        'Gabon',
-        'Gambia',
-        'Ghana',
-        'Guinea-Bissau',
-        'Guinea',
-        'Kenya',
-        'Lesotho',
-        'Liberia',
-        'Libya',
-        'Madagascar',
-        'Malawi',
-        'Mali',
-        'Mauritania',
-        'Mauritius',
-        'Morocco',
-        'Mozambique',
-        'Namibia',
-        'Niger',
-        'Nigeria',
-        'Rwanda',
-        'Sao Tome & Principe',
-        'Senegal',
-        'Seychelles',
-        'Sierra',
-        'Somalia',
-        'South',
-        'South',
-        'Sudan',
-        'Tanzania',
-        'Togo',
-        'Tunisia',
-        'Uganda',
-        'Zambia',
-        'Zimbabwe',
-        "Côte d'Ivoire",
-      ],
-    ],
-    [
-      'Asia',
-      [
-        'Afghanistan',
-        'Armenia',
-        'Azerbaijan',
-        'Bahrain',
-        'Bangladesh',
-        'Bhutan',
-        'Brunei',
-        'Cambodia',
-        'China',
-        'Cyprus',
-        'Georgia',
-        'India',
-        'Indonesia',
-        'Iran',
-        'Iraq',
-        'Israel',
-        'Japan',
-        'Jordan',
-        'Kazakhstan',
-        'Kuwait',
-        'Kyrgyzstan',
-        'Laos',
-        'Lebanon',
-        'Malaysia',
-        'Maldives',
-        'Mongolia',
-        'Myanmar',
-        'Nepal',
-        'North Korea',
-        'Oman',
-        'Pakistan',
-        'Philippines',
-        'Qatar',
-        'Saudi Arabia',
-        'Singapore',
-        'South',
-        'Sri Lanka',
-        'State',
-        'Syria',
-        'Tajikistan',
-        'Thailand',
-        'Timor-Leste',
-        'Turkey',
-        'Turkmenistan',
-        'United Arab Emirates',
-        'Uzbekistan',
-        'Vietnam',
-        'Yemen',
-      ],
-    ],
-    ['Australia', ['Australia']],
-    [
-      'Europe',
-      [
-        'Albania',
-        'Andorra',
-        'Austria',
-        'Belarus',
-        'Belgium',
-        'Bosnia and Herzegovina',
-        'Bulgaria',
-        'Croatia',
-        'Czech Republic',
-        'Denmark',
-        'Estonia',
-        'Finland',
-        'France',
-        'Germany',
-        'Greece',
-        'Holy See',
-        'Hungary',
-        'Iceland',
-        'Ireland',
-        'Italy',
-        'Latvia',
-        'Liechtenstein',
-        'Lithuania',
-        'Luxembourg',
-        'Malta',
-        'Moldova',
-        'Monaco',
-        'Montenegro',
-        'the Netherlands',
-        'North Macedonia',
-        'Norway',
-        'Poland',
-        'Portugal',
-        'Romania',
-        'Russia',
-        'San Marino',
-        'Serbia',
-        'Slovakia',
-        'Slovenia',
-        'Spain',
-        'Sweden',
-        'Switzerland',
-        'Ukraine',
-        'United Kingdom',
-      ],
-    ],
-    ['North America', ['Canada', 'United States', 'Mexico']],
-    [
-      'South America',
-      [
-        'Argentina',
-        'Bolivia',
-        'Brazil',
-        'Chile',
-        'Colombia',
-        'Ecuador',
-        'Guyana',
-        'Paraguay',
-        'Peru',
-        'Suriname',
-        'Uruguay',
-        'Venezuela',
-      ],
-    ],
-  ])
+    import regionTerrain from '$data/regions'
 
-  let country: { index: number; value: string; label: string } | null
+    import city from '$stores/city.store'
+    import player from '$stores/player.store'
+    import see from '$stores/see.store'
 
-  $: if (country) {
-    $city.country = country.value
-  }
+    import type { SelectItem } from '$util/types'
+
+    const continents: Continent[] = [
+        'Africa',
+        'Asia',
+        'Australia',
+        'Europe',
+        'North America',
+        'South America',
+    ]
+    const regions: Map<Continent, Region[]> = new Map([
+        [
+            'Africa',
+            ['Eastern Africa', 'Middle Africa', 'Northern Africa', 'Southern Africa', 'Western Africa'],
+        ],
+        [
+            'Asia',
+            [
+                'Central Asia',
+                'Eastern Asia',
+                'Northern Asia',
+                'Southeastern Asia',
+                'Southern Asia',
+                'Western Asia',
+            ],
+        ],
+        ['Australia', ['Australasia', 'Melanesia', 'Micronesia', 'Oceania', 'Polynesia']],
+        [
+            'Europe',
+            ['Channel Islands', 'Eastern Europe', 'Northern Europe', 'Southern Europe', 'Western Europe'],
+        ],
+        ['North America', ['Central America', 'Northern America']],
+        ['South America', ['Caribbean', 'South America']],
+    ])
+
+    let region: SelectItem<Region> | null
+    let terrain: SelectItem<Terrain> | null
+
+    $: if (region) {
+        $city.region = region.value
+    }
+    $: if (terrain) {
+        $city.terrain = terrain.value
+    }
 </script>
 
 <Card>
-  <p>
-    It seems the people here were impressed with what you told them, {$player.name.first}, and have
-    decided to give you a chance as leader.
-  </p>
-  <p>
-    <b>
-      But where <i>is</i> "here"?
-    </b>
-  </p>
-  <Input name="city-name" placeholder="city name" bind:value={$city.name} />
-
-  {#if $city.name}
-    <p class="player-response">
-      This city is called <b>{$city.name}</b>.
+    <p>
+        It seems the people here were impressed with what you told them, {$player.name.first}, and have
+        decided to give you a chance as leader.
     </p>
+    <p>
+        <b>
+            But where <i>is</i> "here"?
+        </b>
+    </p>
+    <Input name="city-name" placeholder="city name" bind:value={$city.name} />
 
-    <p class="margin-top"><b>On which continent is <b>{$city.name}</b> located?</b></p>
+    {#if $city.name}
+        <p class="player-response">
+            This city is called <b>{$city.name}</b>.
+        </p>
 
-    {#each continents as continent}
-      <Button
-        handler={() => {
+        <p class="margin-top"><b>On which continent is <b>{$city.name}</b> located?</b></p>
+
+        {#each continents as continent}
+            <Button
+                handler={() => {
           $city.continent = continent
-          $city.country = ''
-          country = null
-
-          if (continent == 'Australia') $city.country = 'Australia'
+          $city.region = undefined
+          region = null
         }}
-        focus={$city.continent == continent}>{continent}</Button
-      >
-    {/each}
+                focus={$city.continent === continent}>{continent}</Button
+            >
+        {/each}
 
-    {#if $city.continent && $city.continent != 'Australia'}
-      <div class="margin-top">
-        <p><b>In which former country?</b></p>
+        {#if $city.continent}
+            <div class="margin-top">
+                <p><b>In which region?</b></p>
 
-        <Select
-          items={countries.get($city.continent) ?? []}
-          name="country"
-          placeholder="country"
-          bind:value={country}
-        />
-      </div>
+                <Select
+                    items={regions.get($city.continent) ?? []}
+                    name="region"
+                    placeholder="region"
+                    bind:value={region}
+                />
+            </div>
+        {/if}
+
+        {#if $city.continent}
+            <p class="margin-top player-response">
+                It is located
+                {#if $city.region}
+                    in the <b>{$city.region}</b> region,
+                {/if}
+                on or near the continent of <b>{$city.continent}</b>.
+            </p>
+        {/if}
+
+        {#if $city.region}
+            <div class="margin-top">
+                <p><b>What type of terrain surrounds {$city.name}?</b></p>
+
+                <Select
+                    items={regionTerrain.get($city.region) ?? []}
+                    name="terrain"
+                    placeholder="terrain"
+                    bind:value={terrain}
+                />
+            </div>
+
+            {#if $city.terrain}
+                <p class="margin-top player-response">
+                    {$city.name} is surrounded by {$city.terrain} terrain.
+                </p>
+            {/if}
+        {/if}
     {/if}
-  {/if}
 
-  {#if $city.name}
-    <p class="margin-top player-response">
-      {#if $city.continent}
-        It is located
-        {#if $city.country && $city.country != 'Australia'}
-          in the country of <b>{$city.country}</b>,
-        {/if}
-        on the continent of <b>{$city.continent}</b>.
-      {/if}
-    </p>
-  {/if}
+    {#if $city.name && $city.continent && $city.region}
+        <p class="player-response">
+            This is the city of <b>{$city.name}</b>.
+            {#if $city.continent}
+                It is located
+                {#if $city.terrain}
+                    in a {$city.terrain} area
+                {/if}
+                {#if $city.region}
+                    in <b>{$city.region}</b>,
+                {/if}
+                on or near the continent of <b>{$city.continent}</b>.
+            {/if}
+        </p>
 
-  {#if $city.name && $city.continent && $city.country}
-    <p class="player-response">
-      This is the city of <b>{$city.name}</b>.
-      {#if $city.continent}
-        It is located
-        {#if $city.country && $city.country != 'Australia'}
-          in <b>{$city.country}</b>,
-        {/if}
-        on the continent of <b>{$city.continent}</b>.
-      {/if}
-    </p>
-
-    <div class="margin-top">
-      <Button
-        focus
-        handler={() => {
+        <div class="margin-top">
+            <Button
+                focus
+                handler={() => {
           $see.setup.city = false
           $see.setup.intro = false
           $see.setup.player = false
           $see.ui = true
-        }}>Continue</Button
-      >
-    </div>
-  {/if}
+        }}>Continue
+            </Button
+            >
+        </div>
+    {/if}
 </Card>
