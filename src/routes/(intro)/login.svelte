2673acbbda29bbabd2f0b3c4dce3eab745ea9400
DSoloha <dsoloha@live.com>
2023-12-28 17:37:29 -0500

    Added login page

---
+++ src/routes/(intro)/login.svelte
@@ -0,0 +1,19 @@
+<script>
+  import Button from '$components/Button.svelte'
+  import Card from '$components/Card.svelte'
+  import see from '$stores/see.store'
+</script>
+
+<Card>
+  <p>Future login page</p>
+
+  <Button
+    handler={() => {
+      $see.setup.city = false
+      $see.setup.intro = true
+      $see.setup.login = false
+      $see.setup.player = false
+    }}
+  >← Back
+  </Button>
+</Card>
